36200b6ea63e3aff84041060bcc36cf01c4e5e86
Maire Chew <mairechew@gmail.com>
2017-11-19 14:58:32 -0700

    completed

--- public/scripts/secrets.js
+++ public/scripts/secrets.js
@@ -3,9 +3,30 @@ const url = 'http://localhost:3000/'
 $(() => {
   const $logout = $('#logout-button');
   $logout.click(logout)
+
+  let token = localStorage.getItem('token')
+  let decoded = jwt_decode(token)
+  $.ajax({
+    method: 'GET',
+    url: `${url}users/${decoded.id}/secrets`,
+    headers: {
+      Authorization: `Bearer ${token}`
+    }
+  }).then(secrets => {
+    appendSecrets(secrets)
+  })
 })
 
 const logout = () => {
   localStorage.removeItem('token')
   location.href = '/index.html'
+}
+
+const appendSecrets = (secrets) => {
+  secrets.data.forEach(secret => {
+    $('.secrets').append(
+      `<li>${secret.secret}</li>`
+    )
+  })
+
 }
--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,9 @@
 var express = require('express');
-var queries = require('../db/queries')
+var queries = require('../db/queries');
+var jwt = require('jsonwebtoken')
 
 var router = express.Router();
+require('dotenv').config();
 
 router.get('/', (req, res) => {
   queries.getUsers().then(users => {
@@ -16,9 +18,25 @@ router.get('/:id', (req, res) => {
 });
 
 router.get('/:id/secrets', (req, res) => {
-  queries.getSecretsByUserId(req.params.id).then(secrets => {
-    res.json({data:secrets});
-  })
+  let token = req.headers.authorization.substring(7);
+  let decoded = jwt.verify(token, process.env.TOKEN_SECRET);
+
+  if (req.headers.authorization) {
+    let token = req.headers.authorization.substring(7);
+    let decoded = jwt.verify(token, process.env.TOKEN_SECRET);
+
+    if (decoded.id == req.params.id) {
+      queries.getSecretsByUserId(req.params.id).then(secrets => {
+        res.json({data:secrets});
+      })
+    } else {
+      res.status(401)
+      res.json({error: `You are not authorized/don't have access.`})
+    }
+  } else {
+    res.status(401)
+    res.json({error: `You are not authorized/don't have access.`})
+  }
 });
 
 module.exports = router;
